620d229616152821a1979966e965ddd4320841ab
kamilzachara <kamil.zachara98@gmail.com>
2022-03-31 00:02:22 +0200

    Initial Commit

---
+++ SwiftApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftApp
+//
+//  Created by Kamil Zachara on 31/03/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftApp/SwiftAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftAppApp.swift
+//  SwiftApp
+//
+//  Created by Kamil Zachara on 31/03/2022.
+//
+
+import SwiftUI
+
+@main
+struct SwiftAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
